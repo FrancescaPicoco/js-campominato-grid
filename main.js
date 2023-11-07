2e91a086d785b2bc7ed4ce374af5f629a4132353
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-07 19:21:06 +0100

    prova bonus

--- main.js
+++ main.js
@@ -14,7 +14,30 @@ document.getElementById("btnPlayer").addEventListener("click" , function(){
             let numeroquadtratino = document.createElement("p");
             numeroquadtratino.classList.add("stylenumber");
             numb.appendChild(numeroquadtratino);
-            document.getElementById("numb").innerHTML += `${indexCell}`
+            document.getElementById(numb).innerHTML += `${indexCell}`
+            console.log(numb)
         })
     }
 })
+//(if numerocelle = 81 ==> cambiare classe aggiunta)
+if (numeroCelle == 81) {
+document.getElementById("diffMedia").addEventListener("click" , function(){
+
+    for (let i = 1; i <= medieCelle ; i++) {
+        const indexCell = [i]; 
+        console.log(indexCell);
+        document.getElementById("grid").classList.add("griglia");    //va posto sopra?
+        let quadratino = document.createElement("div");
+        quadratino.classList.add("mediosquare");
+        grid.appendChild(quadratino);  
+        quadratino.addEventListener("click" , function () {
+            this.classList.add("black");
+            console.log(indexCell)
+            let numeroquadtratino = document.createElement("p");
+            numeroquadtratino.classList.add("stylenumber");
+            numb.appendChild(numeroquadtratino);
+            document.querySelector().innerHTML += `${indexCell}`
+        })
+    }
+})
+}
