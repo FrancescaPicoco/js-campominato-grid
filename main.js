1000f06cfdafc7ddfa1cacb356b7f0f1ed5bcc26
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-08 15:40:20 +0100

    rifatto

--- main.js
+++ main.js
@@ -1,44 +1,28 @@
-let numeroCelle = 100
+
 document.getElementById("btnPlayer").addEventListener("click" , function(){
-    
-     for (let i = 1; i <= numeroCelle ; i++) {
-        const indexCell = [i]; 
+    creaGriglia();
+})   
+
+function creaGriglia() {
+    let numeroCelle = 100
+    const gridBuild = document.getElementById("grid");
+    gridBuild.classList.add("griglia");
+    //gridBuild.innerHTML = "";
+    for (let i = 1; i <= numeroCelle ; i++) {
+        let indexCell = creaQuadratino(i);
         console.log(indexCell);
-        document.getElementById("grid").classList.add("griglia");    //va posto sopra?
-        let quadratino = document.createElement("div");
+        gridBuild.appendChild(quadratino);
+    }    
+}
+
+function creaQuadratino(indexCell) {
+    const quadratino = document.createElement("div");
         quadratino.classList.add("square");
-        grid.appendChild(quadratino);  
+        //quadratino.innerHTML += `${indexCell}`
+        quadratino.innerText = (indexCell);
         quadratino.addEventListener("click" , function () {
             this.classList.toggle("black");
-            console.log(indexCell)
-            //let numeroquadtratino = document.createElement("p");
-            //numeroquadtratino.classList.add("stylenumber");
-            //numb.appendChild(numeroquadtratino);
-            quadratino.innerHTML += `${indexCell}`
-            quadratino.classList.add("stylenumber");
-
+            console.log(indexCell)  
+            quadratino.classList.toggle("stylenumber");
         })
-    }
-})
-//(if numerocelle = 81 ==> cambiare classe aggiunta)
-if (numeroCelle == 81) {
-document.getElementById("diffMedia").addEventListener("click" , function(){
-
-    for (let i = 1; i <= medieCelle ; i++) {
-        const indexCell = [i]; 
-        console.log(indexCell);
-        document.getElementById("grid").classList.add("griglia");    //va posto sopra?
-        let quadratino = document.createElement("div");
-        quadratino.classList.add("mediosquare");
-        grid.appendChild(quadratino);  
-        quadratino.addEventListener("click" , function () {
-            this.classList.add("black");
-            console.log(indexCell)
-            let numeroquadtratino = document.createElement("p");
-            numeroquadtratino.classList.add("stylenumber");
-            numb.appendChild(numeroquadtratino);
-            document.querySelector().innerHTML += `${indexCell}`
-        })
-    }
-})
 }
